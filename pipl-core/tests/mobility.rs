482174489b5c3590bba737b3b99bfc6eed927901
Robert Juliano <rovjuvano@gmail.com>
2015-05-04 23:45:04 -0400

    refactor moblity test

--- pipl-core/tests/mobility.rs
+++ pipl-core/tests/mobility.rs
@@ -6,7 +6,9 @@ use std::rc::Rc;
 use pipl::Atom;
 use pipl::Pipl;
 
-fn wx_xy(pipl: &mut Pipl, w: Atom, results: Rc<RefCell<HashMap<&'static str, Atom>>>) {
+type Results = Rc<RefCell<HashMap<&'static str, Atom>>>;
+
+fn wx_xy(pipl: &mut Pipl, w: Atom, results: Results) {
     pipl.add_positive(w, move |pipl, args| {
         let x = pipl.atom();
         args.push(x);
@@ -18,37 +20,41 @@ fn wx_xy(pipl: &mut Pipl, w: Atom, results: Rc<RefCell<HashMap<&'static str, Ato
         });
     });
 }
-fn wz_zz(pipl: &mut Pipl, w: Atom, _results: Rc<RefCell<HashMap<&'static str, Atom>>>) {
+fn wz_zz(pipl: &mut Pipl, w: Atom, results: Results) {
     pipl.add_negative(w, move |pipl, args| {
         let z = args[0];
+        results.borrow_mut().insert("z", z);
         pipl.add_positive(z, move |_pipl, args| {
             args.push(z);
         });
     });
 }
 
-#[test]
-// w(x).x[y].() w[z].z(z).()
-fn positive_reacts_with_negative() {
+fn run<T>(func: T)
+where T: Fn(&mut Pipl, Atom, Results) {
     Pipl::connect(|pipl| {
         let results = Rc::new(RefCell::new(HashMap::new()));
         let w = pipl.atom();
+        func(pipl, w, results.clone());
+        let keys = {let mut t = results.borrow().keys().map(|k| k.clone()).collect::<Vec<&str>>(); t.sort(); t };
+        assert_eq!(vec!["x", "y", "z"], keys);
+        assert_eq!(results.borrow().get("z"), results.borrow().get("x"));
+        assert_eq!(results.borrow().get("z"), results.borrow().get("y"));
+    });
+}
+
+#[test]
+// w(x).x[y].() w[z].z(z).()
+fn positive_reacts_with_negative() {
+    run(|pipl, w, results| {
         wx_xy(pipl, w, results.clone());
         wz_zz(pipl, w, results.clone());
-        let keys = {let mut t = results.borrow().keys().map(|k| k.clone()).collect::<Vec<&str>>(); t.sort(); t };
-        assert_eq!(vec!["x", "y"], keys);
-        assert_eq!(results.borrow().get("x"), results.borrow().get("y"));
     });
 }
 #[test]
 fn negative_reacts_with_positive() {
-    Pipl::connect(|pipl| {
-        let results = Rc::new(RefCell::new(HashMap::new()));
-        let w = pipl.atom();
+    run(|pipl, w, results| {
         wz_zz(pipl, w, results.clone());
         wx_xy(pipl, w, results.clone());
-        let keys = {let mut t = results.borrow().keys().map(|k| k.clone()).collect::<Vec<&str>>(); t.sort(); t };
-        assert_eq!(vec!["x", "y"], keys);
-        assert_eq!(results.borrow().get("x"), results.borrow().get("y"));
     });
 }
