4d2e5da64c3cd712c83823e823543386f1a469a9
Robert Juliano <rovjuvano@gmail.com>
2017-03-26 18:47:00 -0400

    test polyadic prefixes

--- pipl-engine/tests/lib.rs
+++ pipl-engine/tests/lib.rs
@@ -350,3 +350,22 @@ fn no_names() {
     pipl.step();
     assert_eq_results(actual, expected);
 }
+#[test]
+fn polyadic() {
+    // w[x,y].() w(a,b).()
+    let (w, x, y) = (&n(0x77), &n(0x78), &n(0x79));
+    let (a, b) = (&n(0x61), &n(0x62));
+    let mut pipl = Pipl::new();
+    let actual = Rc::new(Results::new());
+    pipl.add(make(vec![read(w, &[x, y])], Terminal, actual.clone()));
+    pipl.add(make(vec![send(w, &[a, b])], Terminal, actual.clone()));
+    let expected = Rc::new(Results::new());
+    let refs_empty = Refs::new();
+    let refs_read = &mut Refs::new();
+    refs_read.set(x.clone(), a.clone());
+    refs_read.set(y.clone(), b.clone());
+    expected.log(f(&read(w, &[x, y])), refs_read.clone());
+    expected.log(f(&send(w, &[a, b])), refs_empty.clone());
+    pipl.step();
+    assert_eq_results(actual, expected);
+}
