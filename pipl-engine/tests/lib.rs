87f393459539e521f2bb59427eee057495b6e795
Robert Juliano <rovjuvano@gmail.com>
2017-04-01 16:29:24 -0400

    test generating new names before sequence as part of suffix

--- pipl-engine/tests/lib.rs
+++ pipl-engine/tests/lib.rs
@@ -106,11 +106,13 @@ impl P {
         sequence(prefix, suffix)
     }
 }
-fn make(prefixes: Vec<P>, suffix: Process, results: Rc<Results>) -> Sequence {
-    let process = prefixes.into_iter().rev().fold(suffix, |suffix, p| {
+fn make_p(prefixes: Vec<P>, suffix: Process, results: Rc<Results>) -> Process {
+    prefixes.into_iter().rev().fold(suffix, |suffix, p| {
         p.logged_sequence(suffix, results.clone())
-    });
-    match process {
+    })
+}
+fn make(prefixes: Vec<P>, suffix: Process, results: Rc<Results>) -> Sequence {
+    match make_p(prefixes, suffix, results) {
         Process::Sequence(sequence) => Rc::try_unwrap(sequence).unwrap(),
         _ => unreachable!(),
     }
@@ -158,6 +160,10 @@ fn assert_eq_refs(left: &Refs, right: &Refs, key: &String, i: usize) {
     for k in left_keys.iter() {
         assert_eq!(left.get(k).raw(), right.get(k).raw(), "results[{:?}][{:?}][{:?}]", key, i, k);
     }
+}
+fn assert_ne_names(a: &Name, b: &Name) {
+    assert_eq!(a.raw(), b.raw());
+    assert_ne!(a, b);
 }
 #[test]
 fn simplest_reaction() {
@@ -421,6 +427,45 @@ fn polyadic() {
     expected.log(f(&send(w, &[a, b])), refs_empty.clone());
     pipl.step();
     assert_eq_results(actual, expected);
+}
+#[test]
+fn new_names_before_sequence() {
+    // w[x].[w, x].y(w).a(b).x(c).() w(a).y[m].a[n].x[o].()
+    let (w, x, y) = (&n(0x77), &n(0x78), &n(0x79));
+    let (a, b, c) = (&n(0x61), &n(0x62), &n(0x63));
+    let (m, n, o) = (&n(0x6d), &n(0x6e), &n(0x6f));
+    let mut pipl = Pipl::new();
+    let actual = Rc::new(Results::new());
+    pipl.add(make(
+        vec![read(w, &[x])],
+        new_names(&[w, x],
+            make_p(vec![send(y, &[w]), send(a, &[b]), send(x, &[c])], Terminal, actual.clone()),
+        ),
+        actual.clone()
+    ));
+    pipl.add(make(vec![send(w, &[a]), read(y, &[m]), read(a, &[n]), read(x, &[o])], Terminal, actual.clone()));
+    let expected = Rc::new(Results::new());
+    let refs_wx = &mut Refs::new();
+    let refs_wa = &mut Refs::new();
+    refs_wx.set(x.clone(), a.clone());
+    expected.log(f(&read(w, &[x])), refs_wx.clone());
+    expected.log(f(&send(w, &[a])), refs_wa.clone());
+    pipl.step();
+    let w2 = &w.dup();
+    refs_wx.set(w.clone(), w2.clone());
+    refs_wx.set(x.clone(), x.dup());
+    refs_wa.set(m.clone(), w2.clone());
+    expected.log(f(&read(y, &[m])), refs_wa.clone());
+    expected.log(f(&send(y, &[w])), refs_wx.clone());
+    pipl.step();
+    refs_wa.set(n.clone(), b.clone());
+    expected.log(f(&read(a, &[n])), refs_wa.clone());
+    expected.log(f(&send(a, &[b])), refs_wx.clone());
+    pipl.step();
+    pipl.step();
+    assert_eq_results(actual.clone(), expected);
+    assert_ne_names(&actual.get(&f(&send(y, &[w]))).get(0).unwrap().get(w), w);
+    assert_ne_names(&actual.get(&f(&read(y, &[m]))).get(0).unwrap().get(m), w);
 }
 #[test]
 fn new_names_before_parallel() {
