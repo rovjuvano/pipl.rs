51240fc08e4a708e5da8878a4dd524d61e14e611
Robert Juliano <rovjuvano@gmail.com>
2018-10-29 04:54:05 -0400

    break apart pipl struct into smaller structs

--- pipl-engine/src/lib.rs
+++ pipl-engine/src/lib.rs
@@ -4,7 +4,6 @@
 use std::collections::BTreeMap;
 use std::collections::BTreeSet;
 use std::fmt;
-
 #[derive(Copy, Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Atom(usize);
 impl fmt::Debug for Atom {
@@ -44,49 +43,16 @@ impl<T: Fn() + 'static> From<T> for Call {
         Call(Box::new(f))
     }
 }
-#[derive(Debug)]
-pub struct Pipl2 {
+#[derive(Debug, Default)]
+pub struct Pipl {
     atoms: Vec<String>,
-    molecules: Vec<AnyMolecule>,
-    waiting_read: BTreeMap<Atom, Vec<Processor>>,
-    waiting_send: BTreeMap<Atom, Vec<Processor>>,
-    ready: BTreeSet<Atom>,
+    solution: Solution,
 }
-#[derive(Debug)]
-struct Processor {
-    molecule: Molecule,
-    map: BTreeMap<Atom, Atom>,
-    //refs
-}
-impl Processor {
-    fn new(molecule: Molecule) -> Self {
-        Processor {
-            molecule,
-            map: BTreeMap::new(),
-        }
-    }
-    #[allow(trivially_copy_pass_by_ref)]
-    fn get(&self, atom: &Atom) -> Atom {
-        *self.map.get(atom).unwrap_or(atom)
-    }
-    fn read(&mut self, keys: &[Atom], values: Vec<Atom>) {
-        for (k, v) in keys.iter().zip(values) {
-            self.map.insert(*k, v);
-        }
-    }
-    fn send(&self, keys: &[Atom]) -> Vec<Atom> {
-        keys.iter().map(|k| self.get(k)).collect()
-    }
-}
-#[allow(new_without_default_derive)]
-impl Pipl2 {
+impl Pipl {
     pub fn new() -> Self {
-        Pipl2 {
+        Pipl {
             atoms: Vec::new(),
-            molecules: vec![AnyMolecule::Terminal],
-            waiting_read: BTreeMap::new(),
-            waiting_send: BTreeMap::new(),
-            ready: BTreeSet::new(),
+            solution: Solution::new(),
         }
     }
     pub fn atom<S: Into<String>>(&mut self, data: S) -> Atom {
@@ -98,71 +64,94 @@ impl Pipl2 {
         Molecule(0)
     }
     pub fn call<I: Into<Call>>(&mut self, call: I, next: Molecule) -> Molecule {
-        self.add_molecule(AnyMolecule::Call(call.into(), next))
+        self.solution
+            .add_molecule(AnyMolecule::Call(call.into(), next))
     }
     pub fn read<'a, I>(&mut self, atom: Atom, atoms: I, next: Molecule) -> Molecule
     where
         I: IntoIterator<Item = &'a Atom>,
     {
-        self.add_molecule(AnyMolecule::Simple(SimpleMolecule::Read(
-            atom,
-            atoms.into_iter().cloned().collect(),
-            next,
-        )))
+        self.solution
+            .add_molecule(AnyMolecule::Simple(SimpleMolecule::Read(
+                atom,
+                atoms.into_iter().cloned().collect(),
+                next,
+            )))
     }
     pub fn send<'a, I>(&mut self, atom: Atom, atoms: I, next: Molecule) -> Molecule
     where
         I: IntoIterator<Item = &'a Atom>,
     {
-        self.add_molecule(AnyMolecule::Simple(SimpleMolecule::Send(
-            atom,
-            atoms.into_iter().cloned().collect(),
-            next,
-        )))
+        self.solution
+            .add_molecule(AnyMolecule::Simple(SimpleMolecule::Send(
+                atom,
+                atoms.into_iter().cloned().collect(),
+                next,
+            )))
     }
     pub fn repeating_read<'a, I>(&mut self, atom: Atom, atoms: I, next: Molecule) -> Molecule
     where
         I: IntoIterator<Item = &'a Atom>,
     {
-        self.add_molecule(AnyMolecule::Repeating(SimpleMolecule::Read(
-            atom,
-            atoms.into_iter().cloned().collect(),
-            next,
-        )))
+        self.solution
+            .add_molecule(AnyMolecule::Repeating(SimpleMolecule::Read(
+                atom,
+                atoms.into_iter().cloned().collect(),
+                next,
+            )))
     }
     pub fn repeating_send<'a, I>(&mut self, atom: Atom, atoms: I, next: Molecule) -> Molecule
     where
         I: IntoIterator<Item = &'a Atom>,
     {
-        self.add_molecule(AnyMolecule::Repeating(SimpleMolecule::Send(
-            atom,
-            atoms.into_iter().cloned().collect(),
-            next,
-        )))
+        self.solution
+            .add_molecule(AnyMolecule::Repeating(SimpleMolecule::Send(
+                atom,
+                atoms.into_iter().cloned().collect(),
+                next,
+            )))
     }
     pub fn parallel<'a, I>(&mut self, molecules: I) -> Molecule
     where
         I: IntoIterator<Item = &'a Molecule>,
     {
         let list = molecules.into_iter().cloned().collect();
-        self.add_molecule(AnyMolecule::Parallel(list))
+        self.solution.add_molecule(AnyMolecule::Parallel(list))
     }
     pub fn choice<'a, I>(&mut self, molecules: I) -> Molecule
     where
         I: IntoIterator<Item = &'a Molecule>,
     {
         let list = molecules.into_iter().cloned().collect();
-        self.add_molecule(AnyMolecule::Choice(list))
+        self.solution.add_molecule(AnyMolecule::Choice(list))
+    }
+    pub fn excite(&mut self, molecule: Molecule) {
+        self.solution.excite(Processor::new(molecule));
+    }
+    pub fn step(&mut self) {
+        self.solution.step();
+    }
+}
+#[derive(Debug, Default)]
+struct Solution {
+    molecules: Vec<AnyMolecule>,
+    ready: ReadySet,
+    reactions: ReactionSet,
+}
+impl Solution {
+    fn new() -> Self {
+        Solution {
+            molecules: vec![AnyMolecule::Terminal],
+            ready: ReadySet::new(),
+            reactions: ReactionSet::new(),
+        }
     }
     fn add_molecule(&mut self, molecule: AnyMolecule) -> Molecule {
         let id = self.molecules.len();
         self.molecules.push(molecule);
         Molecule(id)
     }
-    pub fn excite(&mut self, molecule: Molecule) {
-        self.excite_inner(Processor::new(molecule));
-    }
-    fn excite_inner(&mut self, processor: Processor) {
+    fn excite(&mut self, mut processor: Processor) {
         #[derive(Debug)]
         enum Excite {
             Atom(Atom, bool),
@@ -181,7 +170,7 @@ impl Pipl2 {
                     c.0();
                     Excite::Molecule(*m)
                 }
-                AnyMolecule::Choice(..) => Excite::None,
+                AnyMolecule::Choice(..) => unimplemented!(),
                 AnyMolecule::Terminal => Excite::None,
             }
         } else {
@@ -189,50 +178,35 @@ impl Pipl2 {
         };
         match action {
             Excite::Atom(atom, is_read) => self.excite_simple(atom, processor, is_read),
-            Excite::Molecule(m) => self.excite(m),
-            Excite::Molecules(list) => for m in list {
-                self.excite(m)
-            },
+            Excite::Molecule(m) => {
+                processor.molecule = m;
+                self.excite(processor);
+            }
+            Excite::Molecules(_) => unimplemented!(),
+            // Excite::Molecules(list) => for m in list {
+            //     self.excite(m)
+            // },
             Excite::None => {}
         }
     }
     fn excite_simple(&mut self, atom: Atom, processor: Processor, is_read: bool) {
-        let reads = &mut self.waiting_read;
-        let sends = &mut self.waiting_send;
-        let (this, that) = if is_read {
-            (reads, sends)
-        } else {
-            (sends, reads)
-        };
         let atom = processor.get(&atom);
-        this.entry(atom).or_insert_with(Vec::new).push(processor);
-        use std::collections::btree_map::Entry;
-        if let Entry::Occupied(e) = that.entry(atom) {
-            if !e.get().is_empty() {
-                self.ready.insert(atom);
-            }
+        if is_read {
+            self.reactions.insert_read(atom, processor);
+        } else {
+            self.reactions.insert_send(atom, processor);
+        }
+        if self.reactions.is_waiting(atom) {
+            self.ready.insert(atom);
         }
     }
     pub fn step(&mut self) {
-        if let Some(atom) = self.next() {
-            let (read, send) = {
-                let read = self.waiting_read.get_mut(&atom).unwrap();
-                let send = self.waiting_send.get_mut(&atom).unwrap();
-                if read.is_empty() || send.is_empty() {
-                    self.ready.remove(&atom);
-                }
-                (read.remove(0), send.remove(0))
-            };
+        if let Some(atom) = self.ready.next() {
+            let (read, send) = self.reactions.next(atom);
+            if self.reactions.is_waiting(atom) {
+                self.ready.insert(atom);
+            }
             self.react(read, send);
-        }
-    }
-    fn next(&mut self) -> Option<Atom> {
-        let maybe = self.ready.iter().next().cloned();
-        if let Some(atom) = maybe {
-            self.ready.remove(&atom);
-            maybe
-        } else {
-            None
         }
     }
     fn react(&mut self, mut read: Processor, send: Processor) {
@@ -264,208 +238,91 @@ impl Pipl2 {
         };
         if let Some(m) = copy {
             processor.molecule = m;
-            self.excite_inner(processor);
+            self.excite(processor);
         }
     }
 }
-
-pub mod builders;
-
-// #[derive(Copy, Clone)]
-// pub struct Name(usize);
-// impl fmt::Debug for Name {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         write!(f, "Name({})", self.0)
-//     }
-// }
-// #[derive(Debug, Default)]
-// pub struct Pipl {
-//     processes: Vec<Prefix>,
-//     names: Vec<String>,
-// }
-// impl Pipl {
-//     pub fn new() -> Self {
-//         Self::default()
-//     }
-//     pub fn name<S: Into<String>>(&mut self, s: S) -> Name {
-//         let id = self.names.len();
-//         self.names.push(s.into());
-//         Name(id)
-//     }
-//     fn add_prefix(&mut self, prefix: Prefix) {
-//         self.processes.push(prefix);
-//     }
-// }
-// #[derive(Debug)]
-// enum Channel {
-//     Read(Name),
-//     Send(Name),
-// }
-// #[derive(Debug)]
-// pub struct Prefix(Channel, Molecule);
-// #[derive(Debug)]
-// enum Channel {
-//     Read(Name),
-//     Send(Name),
-// }
-// #[derive(Debug, Default)]
-// pub struct Tua {
-//     steps: Vec<Channel>,
-// }
-// impl Tua {
-//     fn new() -> Self {
-//         Self::default()
-//     }
-//     pub fn read(mut self, name: Name) -> Self {
-//         self.steps.push(Channel::Read(name));
-//         self
-//     }
-//     pub fn send(mut self, name: Name) -> Self {
-//         self.steps.push(Channel::Send(name));
-//         self
-//     }
-//     pub fn launch(mut self, pipl: &mut Pipl) {
-//         let process =
-//             self.steps
-//                 .drain(..)
-//                 .fold(
-//                     Suffix::Terminal(Terminal::Sequence),
-//                     |suffix, step| match step {
-//                         Channel::Read(name) => Suffix::Read(Box::new(Read(name, suffix))),
-//                         Channel::Send(name) => Suffix::Send(Box::new(Send(name, suffix))),
-//                     },
-//                 );
-//         match process {
-//             Suffix::Read(prefix) => pipl.add_read(*prefix),
-//             Suffix::Send(prefix) => pipl.add_send(*prefix),
-//             _ => {}
-//         };
-//     }
-// }
-// enum Suffix {
-//     Read(Box<Read>),
-//     Send(Box<Send>),
-//     Terminal(Terminal),
-// }
-// impl fmt::Debug for Suffix {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         match self {
-//             Suffix::Read(x) => x.fmt(f),
-//             Suffix::Send(x) => x.fmt(f),
-//             Suffix::Terminal(x) => x.fmt(f),
-//         }
-//     }
-// }
-// pub struct Read(Name, Suffix);
-// impl Read {
-//     pub fn read(self, name: Name) -> Read {
-//         Read(name, Suffix::Read(Box::new(self)))
-//     }
-//     pub fn send(self, name: Name) -> Send {
-//         Send(name, Suffix::Read(Box::new(self)))
-//     }
-//     pub fn choice(self, choice: &mut Choice) {
-//         choice.add_read(self)
-//     }
-//     pub fn launch(self, pipl: &mut Pipl) {
-//         pipl.add_read(self);
-//     }
-//     pub fn parallel(self, parallel: &mut Parallel) {
-//         parallel.add_read(self);
-//     }
-// }
-// impl fmt::Debug for Read {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         if let Suffix::Terminal(Terminal::Sequence) = self.1 {
-//             write!(f, "Read({:?})", self.0)
-//         } else {
-//             f.debug_tuple("Read").field(&self.0).field(&self.1).finish()
-//         }
-//     }
-// }
-// #[derive(Debug)]
-// pub struct Send(Name, Suffix);
-// impl Send {
-//     pub fn read(self, name: Name) -> Read {
-//         Read(name, Suffix::Send(Box::new(self)))
-//     }
-//     pub fn send(self, name: Name) -> Send {
-//         Send(name, Suffix::Send(Box::new(self)))
-//     }
-//     pub fn choice(self, choice: &mut Choice) {
-//         choice.add_send(self)
-//     }
-//     pub fn launch(self, pipl: &mut Pipl) {
-//         pipl.add_send(self);
-//     }
-//     pub fn parallel(self, parallel: &mut Parallel) {
-//         parallel.add_send(self)
-//     }
-// }
-// // impl fmt::Debug for Send {
-// //     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-// //         if let Suffix::Terminal(Terminal::Sequence) = self.1 {
-// //             write!(f, "Send({:?})", self.0)
-// //         } else {
-// //             f.debug_tuple("Send").field(&self.0).field(&self.1).finish()
-// //         }
-// //     }
-// // }
-// #[derive(Debug, Default)]
-// pub struct Choice {
-//     processes: Vec<Prefix>,
-// }
-// impl Choice {
-//     pub fn new() -> Self {
-//         Self::default()
-//     }
-//     pub fn prefix(self) -> Terminal {
-//         Terminal::Choice(self)
-//     }
-//     fn add_read(&mut self, read: Read) {
-//         self.processes.push(Prefix::Read(read));
-//     }
-//     fn add_send(&mut self, send: Send) {
-//         self.processes.push(Prefix::Send(send));
-//     }
-// }
-// #[derive(Debug, Default)]
-// pub struct Parallel {
-//     processes: Vec<Prefix>,
-// }
-// impl Parallel {
-//     pub fn new() -> Self {
-//         Self::default()
-//     }
-//     pub fn prefix(self) -> Terminal {
-//         Terminal::Parallel(self)
-//     }
-//     fn add_read(&mut self, read: Read) {
-//         self.processes.push(Prefix::Read(read));
-//     }
-//     fn add_send(&mut self, send: Send) {
-//         self.processes.push(Prefix::Send(send));
-//     }
-// }
-// pub enum Terminal {
-//     Choice(Choice),
-//     Parallel(Parallel),
-//     Sequence,
-// }
-// impl Terminal {
-//     pub fn send(self, name: Name) -> Send {
-//         Send(name, Suffix::Terminal(self))
-//     }
-//     pub fn read(self, name: Name) -> Read {
-//         Read(name, Suffix::Terminal(self))
-//     }
-// }
-// impl fmt::Debug for Terminal {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         match self {
-//             Terminal::Choice(x) => x.fmt(f),
-//             Terminal::Parallel(x) => x.fmt(f),
-//             Terminal::Sequence => f.write_str("Terminal"),
-//         }
-//     }
-// }
+#[derive(Debug)]
+struct Processor {
+    molecule: Molecule,
+    map: BTreeMap<Atom, Atom>,
+}
+impl Processor {
+    fn new(molecule: Molecule) -> Self {
+        Processor {
+            molecule,
+            map: BTreeMap::new(),
+        }
+    }
+    #[allow(trivially_copy_pass_by_ref)]
+    fn get(&self, atom: &Atom) -> Atom {
+        *self.map.get(atom).unwrap_or(atom)
+    }
+    fn read(&mut self, keys: &[Atom], values: Vec<Atom>) {
+        for (k, v) in keys.iter().zip(values) {
+            self.map.insert(*k, v);
+        }
+    }
+    fn send(&self, keys: &[Atom]) -> Vec<Atom> {
+        keys.iter().map(|k| self.get(k)).collect()
+    }
+}
+#[derive(Debug, Default)]
+struct ReactionSet {
+    set: BTreeMap<Atom, (Vec<Processor>, Vec<Processor>)>,
+}
+impl ReactionSet {
+    fn new() -> Self {
+        ReactionSet {
+            set: BTreeMap::new(),
+        }
+    }
+    fn insert_read(&mut self, atom: Atom, processor: Processor) {
+        self.set
+            .entry(atom)
+            .or_insert_with(|| (Vec::new(), Vec::new()))
+            .0
+            .push(processor);
+    }
+    fn insert_send(&mut self, atom: Atom, processor: Processor) {
+        self.set
+            .entry(atom)
+            .or_insert_with(|| (Vec::new(), Vec::new()))
+            .1
+            .push(processor);
+    }
+    fn is_waiting(&self, atom: Atom) -> bool {
+        if let Some(x) = self.set.get(&atom) {
+            !x.0.is_empty() && !x.1.is_empty()
+        } else {
+            false
+        }
+    }
+    fn next(&mut self, atom: Atom) -> (Processor, Processor) {
+        let mut x = self.set.remove(&atom).unwrap();
+        (x.0.remove(0), x.1.remove(0))
+    }
+}
+#[derive(Debug, Default)]
+struct ReadySet {
+    set: BTreeSet<Atom>,
+}
+impl ReadySet {
+    fn new() -> Self {
+        ReadySet {
+            set: BTreeSet::new(),
+        }
+    }
+    fn insert(&mut self, atom: Atom) {
+        self.set.insert(atom);
+    }
+    fn next(&mut self) -> Option<Atom> {
+        let maybe = self.set.iter().next().cloned();
+        if let Some(atom) = maybe {
+            self.set.remove(&atom);
+            maybe
+        } else {
+            None
+        }
+    }
+}
--- pipl-engine/tests/cases.rs
+++ pipl-engine/tests/cases.rs
@@ -1,13 +1,12 @@
 extern crate pipl_engine;
-// use pipl_engine::builders::inverted::Builder;
-use pipl_engine::Pipl2;
+use pipl_engine::Pipl;
 use std::cell::RefCell;
 use std::rc::Rc;
 #[test]
 #[ignore]
 fn simplest_reaction() {
     // w[x] w(a)
-    let pipl = &mut Pipl2::new();
+    let pipl = &mut Pipl::new();
     let a = pipl.atom("a");
     let w = pipl.atom("w");
     let x = pipl.atom("x");
@@ -32,7 +31,7 @@ fn simplest_reaction() {
 #[test]
 fn simplest_mobility() {
     // w(x).x[y].() w[z].z(z).()
-    let pipl = &mut Pipl2::new();
+    let pipl = &mut Pipl::new();
     let w = pipl.atom("0");
     let x = pipl.atom("1");
     let y = pipl.atom("2");
@@ -48,73 +47,3 @@ fn simplest_mobility() {
     // pipl.step();
     println!("{:#?}", pipl);
 }
-// #[test]
-// fn swx_rxy__rwz_szz() {
-//     // w(x).x[y].() w[z].z(z).()
-//     let pipl = &mut Pipl2::new();
-//     let w = pipl.atom("w");
-//     let x = pipl.atom("x");
-//     let y = pipl.atom("y");
-//     let z = pipl.atom("z");
-//     let rwz = pipl.read(w, &[&x]);
-//     let swx = pipl.send(w, &[z]);
-//     let rxy = pipl.read(x, &[y]);
-//     let szz = pipl.send(z, &[z]);
-//     pipl.reaction(rwz, szz);
-//     pipl.reaction(swx, rxy);
-//     pipl.excite(rwz);
-//     pipl.excite(swx);
-//     println!("{:#?}", pipl);
-// }
-// #[test]
-// #[ignore]
-// fn simplest_reaction() {
-//     // w[x] w(a)
-//     let pipl = &mut Pipl2::new();
-//     let builder = &mut Builder::new();
-//     let a = pipl.atom("a"); // 0
-//     let b = pipl.atom("b"); // 1
-//     let c = pipl.atom("c"); // 2
-//     let d = pipl.atom("d"); // 3
-//     let e = pipl.atom("e"); // 4
-//     let f = pipl.atom("f"); // 5
-//     let mut p = Builder::parallel();
-//     Builder::sequence().send(d).read(c).parallel(&mut p);
-//     Builder::sequence().read(d).send(c).parallel(&mut p);
-//     let mut p2 = Builder::parallel();
-//     Builder::sequence().send(f).read(e).parallel(&mut p2);
-//     Builder::sequence().read(f).send(e).parallel(&mut p2);
-//     p2.prefix().send(d).read(c).parallel(&mut p);
-//     p.prefix().send(b).read(a).launch(builder);
-//     let mut ch = Builder::choice();
-//     Builder::sequence().send(d).read(c).choice(&mut ch);
-//     Builder::sequence().read(d).send(c).choice(&mut ch);
-//     ch.prefix().send(b).read(a).launch(builder);
-//     let s = Builder::sequence().send(f);
-//     let ss = s.send(f);
-//     ss.send(f).launch(builder);
-//     Builder::sequence().send(a).read(b).launch(builder);
-//     println!("{:#?}", builder);
-//     assert_eq!(true, false);
-// }
-/*
-use helpers::*;
-#[test]
-fn simplest_reaction() {
-    // w[x] w(a)
-    let mut pipl = Pipl::new();
-    names!(|pipl| { w x a });
-    let actual = &Rc::new(Results::new());
-    let mut builder = PiplBuilder::new();
-    builder.read(w).names(&[x]).call(log("w[x]", actual));
-    builder.send(w).names(&[a]).call(log("w(a)", actual));
-    builder.apply(&mut pipl);
-    let expected = &Rc::new(Results::new());
-    let refs = &mut Refs::new();
-    expected.log("w(a)", refs.clone());
-    refs.set(x.clone(), a.clone());
-    expected.log("w[x]", refs.clone());
-    pipl.step();
-    assert_eq_results(&pipl, actual, expected);
-}
-*/
